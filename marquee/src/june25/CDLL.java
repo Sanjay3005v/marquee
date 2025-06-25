8057656a7666d59a4f6ac68505d94311e7b021ec
SANJAY V <sanjay3005v@gmail.com>
2025-06-25 09:27:24 +0530

    june25

---
+++ marquee/src/june25/CDLL.java
@@ -0,0 +1,201 @@
+package june25;
+
+public class CDLL {
+	public static void main(String[] args) {
+		CircularDoublyLinkedList list = new CircularDoublyLinkedList();
+		list.append(10);
+		list.append(20);
+		list.append(30);
+		list.prepend(10);
+		list.prepend(20);
+		list.prepend(30);
+		list.insert(50, 3);
+		list.deleteFirst();
+		list.deleteLast();
+		list.print();
+		list.delete(3);
+		list.print();
+		list.printReverse();
+		System.out.println(list.size());
+	}
+}
+
+class CircularDoublyLinkedList {
+	Node head;
+	Node tail;
+	int size;
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void append(int val) {
+		Node node = new Node(val);
+		if (isEmpty()) {
+			head = tail = node;
+			head.next = head;
+			head.prev = head;
+
+		} else {
+			tail.next = node;
+			node.prev = tail;
+			tail = node;
+			tail.next = node;
+			head.prev = tail;
+		}
+		size++;
+		return;
+	}
+
+	public void prepend(int val) {
+		Node node = new Node(val);
+		if (isEmpty()) {
+			head = tail = node;
+			head.next = head;
+			tail.prev = head;
+		} else {
+			node.next = head;
+			head.prev = node;
+			head = node;
+			tail.next = head;
+			head.prev = tail;
+		}
+		size++;
+		return;
+	}
+
+	public boolean insert(int val, int index) {
+		if (index < 0) {
+			return false;
+		}
+		if (index == 0) {
+			prepend(val);
+			return true;
+		}
+		if (index >= size) {
+			append(val);
+			return true;
+		}
+		Node node = new Node(val);
+		if (isEmpty()) {
+			head = tail = node;
+		} else {
+			Node temp = head;
+			for (int i = 0; i < index - 1; i++) {
+				temp = temp.next;
+			}
+			node.next = temp.next;
+			node.prev = temp;
+			temp.next = node;
+			node.next.prev = node;
+		}
+		size++;
+		return true;
+	}
+
+	public boolean deleteFirst() {
+		if (isEmpty()) {
+			return false;
+		}
+		if (size == 0) {
+			head = tail = null;
+		} else {
+			head = head.next;
+			head.prev = tail;
+			tail.next = head;
+		}
+		size--;
+		return true;
+	}
+
+	public boolean deleteLast() {
+		if (isEmpty()) {
+			return false;
+		}
+		if (size == 1) {
+			head = tail = null;
+		} else {
+			tail = tail.prev;
+			tail.next = head;
+			head.prev = tail;
+		}
+		size--;
+		return true;
+	}
+
+	public boolean delete(int index) {
+		if (isEmpty() || index < 0 || index >= size) {
+			return false;
+		}
+		if (size() == 1) {
+			head = tail = null;
+
+		} else if (index == 0) {
+			deleteFirst();
+		} else if (index == size - 1) {
+			deleteLast();
+		} else {
+			Node temp = head;
+			for (int i = 0; i < index - 1; i++) {
+				temp = temp.next;
+			}
+			temp.next = temp.next.next;
+			temp.next.prev = temp;
+		}
+		size--;
+		return true;
+	}
+
+	public void print() {
+		if (isEmpty()) {
+			return;
+		} else {
+			Node node = head;
+			do {
+				System.out.print(node.val + " ");
+				node = node.next;
+			} while (node != head);
+			System.out.println();
+			return;
+		}
+	}
+
+	public void printReverse() {
+		if (isEmpty()) {
+			return;
+		} else {
+			Node node = tail;
+			do {
+				System.out.print(node.val + " ");
+				node = node.prev;
+			} while (node != tail);
+			System.out.println();
+			return;
+		}
+	}
+
+	public int getVal(int index) {
+		if (index >= 0 && index < size) {
+			Node temp = head;
+			for (int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+			return temp.val;
+		}
+		return -1;
+	}
+}
+
+class Node {
+	int val;
+	Node next;
+	Node prev;
+
+	Node(int val) {
+		this.val = val;
+	}
+}
