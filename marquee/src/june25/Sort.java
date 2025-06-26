edc1a22167d8ff47b7716e0cc62cc4b97da6e57e
SANJAY V <sanjay3005v@gmail.com>
2025-06-26 08:23:28 +0530

    june26

---
+++ marquee/src/june25/Sort.java
@@ -0,0 +1,42 @@
+package june25;
+
+import java.util.Arrays;
+
+public class Sort {
+	public static void main(String[] args) {
+		int[] arr = { 9, 5, 8, 7, 6, 4, 2, 1, 3 };
+		bubble(arr);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	public static void bubble(int[] arr) {
+		boolean swap;
+		for (int i = 0; i < arr.length - 1; i++) {
+			swap = false;
+			for (int j = 0; j < arr.length - i - 1; j++) {
+				if (arr[j] > arr[j + 1]) {
+					int temp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = temp;
+					swap = true;
+				}
+			}
+			if (!swap) {
+				break;
+			}
+		}
+	}
+	public static void selection(int[] arr) {
+		 for (int i = 0; i < arr.length - 1; i++) {
+			 int min = i;
+	         for (int j = i + 1; j < arr.length; j++) {
+	        	 if (arr[j] < arr[min]) {
+	        		 min = j;
+	        	 }
+	         }
+	         int temp = arr[min];
+	         arr[min] = arr[i];
+	         arr[i] = temp;
+		 }
+	}
+}
