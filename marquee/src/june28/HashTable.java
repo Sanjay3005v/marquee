b1899e1cf5c30339d1be940c2d5886b0bb9de8f6
SANJAY V <sanjay3005v@gmail.com>
2025-07-02 09:35:11 +0530

    july2

--- marquee/src/june28/HashTable.java
+++ marquee/src/june28/HashTable.java
@@ -4,44 +4,43 @@ public class HashTable<T> {
 	public static void main(String[] args) {
 		HashTable<Object> ht = new HashTable<>();
 
-		System.out.println("==== Inserting mixed types ====");
-		System.out.println("Insert 42: " + ht.insert(42));
-		System.out.println("Insert \"Batman\": " + ht.insert("Batman"));
-		System.out.println("Insert 3.14: " + ht.insert(3.14));
-		System.out.println("Insert 'A': " + ht.insert('A'));
-		System.out.println("Insert true: " + ht.insert(true));
-		System.out.println("Insert null: " + ht.insert(null));
+		System.out.println("inserting");
+		System.out.println(ht.insert(42));
+		System.out.println(ht.insert("Batman"));
+		System.out.println(ht.insert(0.14));
+		System.out.println(ht.insert('A'));
+		System.out.println(ht.insert(true));
+		System.out.println(ht.insert(null));
 
 		ht.display();
 
-		System.out.println("==== Duplicate Insertions ====");
-		System.out.println("Insert duplicate 42: " + ht.insert(42));
-		System.out.println("Insert duplicate \"Batman\": " + ht.insert("Batman"));
-		System.out.println("Insert duplicate null: " + ht.insert(null));
+		System.out.println("duplicate check");
+		System.out.println(ht.insert(42));
+		System.out.println(ht.insert("Batman"));
+		System.out.println(ht.insert(null));
 
 		ht.display();
 
-		System.out.println("==== Contains Checks ====");
-		System.out.println("Contains 42: " + ht.contains(42));
-		System.out.println("Contains \"Batman\": " + ht.contains("Batman"));
-		System.out.println("Contains 3.14: " + ht.contains(3.14));
-		System.out.println("Contains 'A': " + ht.contains('A'));
-		System.out.println("Contains true: " + ht.contains(true));
-		System.out.println("Contains null: " + ht.contains(null));
-		System.out.println("Contains \"Joker\": " + ht.contains("Joker"));
-		System.out.println("Contains 999: " + ht.contains(999));
+		System.out.println("contains");
+		System.out.println( ht.contains(42));
+		System.out.println( ht.contains("Batman"));
+		System.out.println(ht.contains(0.14));
+		System.out.println(ht.contains('A'));
+		System.out.println(ht.contains(true));
+		System.out.println(ht.contains(null));
+		System.out.println(ht.contains("Joker"));
+		System.out.println(ht.contains(545));
 
-		System.out.println("==== Deletion Tests ====");
-		System.out.println("Delete 3.14: " + ht.delete(3.14));
-		System.out.println("Delete \"Batman\": " + ht.delete("Batman"));
-		System.out.println("Delete null: " + ht.delete(null));
-		System.out.println("Delete 999: " + ht.delete(999));
-		System.out.println("Delete \"Joker\": " + ht.delete("Joker"));
-		System.out.println("Delete null again: " + ht.delete(null));
+		System.out.println("deleting");
+		System.out.println(ht.delete(0.14));
+		System.out.println(ht.delete("Batman"));
+		System.out.println(ht.delete(null));
+		System.out.println(ht.delete(4525));
+		System.out.println(ht.delete("Joker"));
+		System.out.println(ht.delete(null));
 
 		ht.display();
 
-		System.out.println(" All test cases executed.");
 	}
 
 	Node<T>[] arr = new Node[10];
@@ -79,6 +78,9 @@ public class HashTable<T> {
 	}
 
 	public boolean insert(T key) {
+		if((size/arr.length)>0.75) {
+			
+		}
 		int index = hashFunction(key);
 		Node<T> node = arr[index];
 
@@ -103,14 +105,14 @@ public class HashTable<T> {
 	}
 
 	public boolean delete(T key) {
-		int index = hashFunction(key);
-		Node<T> curr = arr[index];
+
+		Node<T> curr = arr[hashFunction(key)];
 		Node<T> prev = null;
 
 		while (curr != null) {
 			if ((key == null && curr.val == null) || (key != null && key.equals(curr.val))) {
 				if (prev == null) {
-					arr[index] = curr.next;
+					arr[hashFunction(key)] = curr.next;
 				} else {
 					prev.next = curr.next;
 				}
