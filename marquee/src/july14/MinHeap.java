6467f44a5e7e897d34949c8f1db88598ad3bef12
SANJAY V <sanjay3005v@gmail.com>
2025-07-15 07:58:38 +0530

    july14

--- marquee/src/july14/MinHeap.java
+++ marquee/src/july14/MinHeap.java
@@ -5,7 +5,7 @@ public class MinHeap {
 	int size;
 
 	MinHeap(int size) {
-		arr = new int[size];
+		arr = new int[this.size];
 	}
 
 	public void display() {
@@ -21,32 +21,68 @@ public class MinHeap {
 	public boolean isFull() {
 		return size == arr.length;
 	}
-	
+
 	public void add(int val) {
-		if(isFull()) {
+		if (isFull()) {
 			System.out.println("Heap is Full");
 			return;
 		}
-		arr[++size]=val;
+		arr[++size] = val;
 		heapifyBottomUp(size);
 	}
+
 	public void heapifyBottomUp(int index) {
-		if(index<=1) {
+		if (index <= 1) {
 			return;
 		}
-		int parent = index/2;
-		if(arr[index]<arr[parent]) {
-			arr[parent] = (arr[parent]+arr[index])-(arr[index]=arr[parent]);
+		int parent = index / 2;
+		if (arr[index] < arr[parent]) {
+			arr[parent] = (arr[parent] + arr[index]) - (arr[index] = arr[parent]);
 		}
 		heapifyBottomUp(parent);
 	}
+
+	public Integer peek() {
+		return isEmpty() ? null : arr[1];
+	}
+
+	public void delete() {
+		if (isEmpty()) {
+			System.out.println("Heap is empty");
+			return;
+		}
+		arr[1] = arr[size--];
+		heapifyTopDown(1);
+	}
+
+	public void heapifyTopDown(int index) {
+		int left = index * 2;
+		int right = (index * 2) + 1;
+		if (size < left) {
+			return;
+		}
+		if (size == left && arr[left] < arr[index]) {
+			arr[left] = (arr[left] + arr[index]) - (arr[index] = arr[left]);
+		} else {
+			int min = arr[left] < arr[right] ? left : right;
+			if (arr[index] > arr[min]) {
+				arr[min] = (arr[min] + arr[index]) - (arr[index] = arr[min]);
+				heapifyTopDown(min);
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		MinHeap hp = new MinHeap(10);
+		System.out.println(hp.peek());
 		hp.add(10);
 		hp.add(12);
 		hp.add(5);
 		hp.add(19);
 		hp.add(3);
+		System.out.println(hp.peek());
 		hp.display();
+//		hp.delete();
+//		hp.display();
 	}
 }
